359d0e58f6174a4a348413d497f9651febef0d54
AdeBots <190976863+AdeBots@users.noreply.github.com>
2025-01-10 14:23:50 +0100

    Write win strategy

--- script.js
+++ script.js
@@ -1,28 +1,76 @@
 
 function gameboard(){
-    const gameboard = [["","",""], ["","",""], ["","",""]];
+    const board = [["","",""], ["","",""], ["","",""]];
     //first box = gameboard[0][0]
 
-    const getBoard = () => gameboard;
+    const getBoard = () => board;
 
-    const inputValue = (column, player) => {
+    const inputValue = (row, column, player) => {
+        if (board[row][column] === "") {
+            board[row][column] = player;
+        }  
+    };
+
+    function checkWin(){
+        if(board[0][0] === board[0][1] === board[0][2] === "X" || board[0][0] === board[1][0] === board[2][0] === "X"
+            || board[2][0] === board[2][1] === board[2][2] === "X" || board[0][2] === board[1][2] === board[2][2] === "X"
+            || board[0][1] === board[1][1] === board[2][1] === "X" || board[1][0] === board[1][1] === board[1][2] === "X"
+            || board[0][0] === board[1][1] === board[2][2] === "X" || board[0][2] === board[1][1] === board[2][0] === "X"){ 
+           console.log("X Wins");
+        }
+        else if(board[0][0] === board[0][1] === board[0][2] === "O"){
+            console.log("O Wins");
+        }
+        else if("){
+            console.log("X Wins");
+        }
+        else if(board[0][0] === board[0][1] === board[0][2] === "X"){
+            console.log("X Wins");
+        }
+        else if(board[0][0] === board[0][1] === board[0][2] === "X"){
+            console.log("X Wins");
+        }
+        else if(board[0][0] === board[0][1] === board[0][2] === "X"){
+            console.log("X Wins");
+        }
+        else if(board[0][0] === board[0][1] === board[0][2] === "X"){
+            console.log("X Wins");
+        }
+        else if(board[0][0] === board[0][1] === board[0][2] === "X"){
+            console.log("X Wins");
+        }
+        else if(board[0][0] === board[0][1] === board[0][2] === "X"){
+            console.log("X Wins");
+        }
         
     }
 
+    function checkTie(){
+
+    }
+
+    return { getBoard, inputValue, checkWin, checkTie };
 }
 
 function players(){
     let playerOneName;
     let playerTwoName;
-    function getPlayerName(){
+    const playerOneToken = "X";
+    const playerTwoToken = "O"; 
+
+    function getPlayerNames(){
         playerOneName =prompt("Input the first player's name");
-        playerTwoName =prompt("Input the second player's name")
+        playerTwoName =prompt("Input the second player's name");
     }
-    let playerOneToken = "X";
-    let playerTwoToken = "O"; 
-    return {getPlayerName(), }
+    return { getPlayerNames, playerOneToken, playerTwoToken };
 }
 
 function displayConttroller(){
+    const renderBoard = () => {
+        const board = gameboard.getBoard();
+    };
 
+    return { renderBoard };
 }
+
+displayConttroller.renderBoard();
