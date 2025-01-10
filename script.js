794e0d5d407ca5f768c60b5ba644e43bb83de728
AdeBots <190976863+AdeBots@users.noreply.github.com>
2025-01-10 13:40:57 +0100

    Script not working

---
+++ script.js
@@ -0,0 +1,28 @@
+
+function gameboard(){
+    const gameboard = [["","",""], ["","",""], ["","",""]];
+    //first box = gameboard[0][0]
+
+    const getBoard = () => gameboard;
+
+    const inputValue = (column, player) => {
+        
+    }
+
+}
+
+function players(){
+    let playerOneName;
+    let playerTwoName;
+    function getPlayerName(){
+        playerOneName =prompt("Input the first player's name");
+        playerTwoName =prompt("Input the second player's name")
+    }
+    let playerOneToken = "X";
+    let playerTwoToken = "O"; 
+    return {getPlayerName(), }
+}
+
+function displayConttroller(){
+
+}
